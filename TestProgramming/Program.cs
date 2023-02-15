cb3de382c873bf8d1a2c6198619fd76e1840cf45
Saharat <Saharat.Mee@turbo.co.th>
2023-02-15 16:19:09 +0700

    test

--- TestProgramming/Program.cs
+++ TestProgramming/Program.cs
@@ -6,7 +6,7 @@ using Colors.Net.StringColorExtensions;
 
 namespace FileDeleteProgram
 {
-    class Program
+    class Program //testtttttttttttttttttttttttttttttttttttttttttttttttt
     {
         static void Main(string[] args)
         {
